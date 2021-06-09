919e6acdc1256b38928b2d269a02176eee8233d6
sneharini <67590369+sneharini@users.noreply.github.com>
2021-06-08 21:48:07 -0700

    Create class_attendence.py

---
+++ class_attendence.py
@@ -0,0 +1,39 @@
+import cv2
+import numpy as np
+import face_recognition
+import os
+from datetime import datetime
+
+path = 'Image_attendence'
+images = []
+classNames = []
+myList = os.listdir(path)
+print(myList)
+for cl in myList:
+    curImg = cv2.imread(f'{path}/{cl}')
+    images.append(curImg)
+    classNames.append(os.path.splitext(cl)[0])
+print(classNames)
+
+def findEncodings(images):
+    encodelist = []
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodelist.append(encode)
+    return encodelist
+
+def markAttendence(name):
+    with open('Attendence.csv','r+') as f:
+        myDataList = f.readlines()
+        nameList = []
+        for line in myDataList:
+            entry = line.split(',')
+            nameList.append(entry[0])
+        if name not in nameList:
+            now = datetime.now()
+            dtstring = now.strftime('%H:%M:%S')
+            f.writelines(f'\n{name},{dtstring}')
+
+
+
